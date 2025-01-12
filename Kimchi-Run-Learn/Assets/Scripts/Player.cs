3536d4888e061310f6c09ed322f790ffb05eb15b
KKJ1000 <kkj1733n@gmail.com>
2025-01-12 21:59:53 +0900

    [Feat] Player Hit & Heal

--- Kimchi-Run-Learn/Assets/Scripts/Player.cs
+++ Kimchi-Run-Learn/Assets/Scripts/Player.cs
@@ -9,7 +9,12 @@ public class Player : MonoBehaviour
     public Rigidbody2D playerRigidBody;
     public Animator playerAnimator;
 
+    public BoxCollider2D playerCollider;
+
     private bool isGrounded = true;
+
+    private int lives = 3;
+    private bool isInvincible = false;
 
     void Update()
     {
@@ -20,6 +25,39 @@ public class Player : MonoBehaviour
             playerAnimator.SetInteger("state", 1);
         }
     }
+
+    void KillPlayer()
+    {
+        playerCollider.enabled = false;
+        playerAnimator.enabled = false;
+        playerRigidBody.AddForceY(jumpForce, ForceMode2D.Impulse);
+    }
+
+    void Hit()
+    {
+        lives -= 1; // 생명력 1 감소
+        if (lives == 0)
+        {
+            KillPlayer();
+        }
+    }
+
+    void Heal()
+    {
+        lives = Mathf.Min(3, lives + 1);
+    }
+
+    void StartInvincible()
+    {
+        isInvincible = true;
+        Invoke("StopInvincible", 3f);
+    }
+
+    void StopInvincible()
+    {
+        isInvincible = false;
+    }
+
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -37,15 +75,21 @@ public class Player : MonoBehaviour
     {
         if(collision.CompareTag("ENEMY"))
         {
-
+            if(!isInvincible)
+            {
+                Destroy(collision.gameObject);
+                Hit();
+            }
         }
         else if(collision.CompareTag("FOOD"))
         {
-
+            Destroy(collision.gameObject);
+            Heal();
         }
         else if(collision.CompareTag("GOLDEN"))
         {
-
+            Destroy(collision.gameObject);
+            StartInvincible();
         }
     }
 }
